a6e1ae1f5275a87279655837ba66fc1784271a64
Brian Weinthal <76851117+189427@users.noreply.github.com>
2022-09-17 17:43:13 -0400

    code

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-$("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
-
-
+$("#currentDay").text(moment().format("dddd MMMM Do YYYY"));
 
 var save = $(".btn");
 
 function saveButton(event) {
-    console.log($(this).siblings())
+  console.log($(this).siblings());
 }
 
-save.on("click", saveButton)
+save.on("click", saveButton);
+
+$(".btn").click(function (event) {
+  event.preventDefault();
+  var value = $(this).siblings(".time-block").val();
+  var time = $(this).parent().attr("id").split("-")[1];
+  localStorage.setItem(time, value);
+});
